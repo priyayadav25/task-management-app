1e2c4e9c7a40c4921cf49144691356003c954afd
priyayadav25 <priyay2519@gmail.com>
2026-06-15 21:36:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,25 +8,44 @@ app.use(express.json());
 
 let tasks = [];
 
+// Home Route
+app.get("/", (req, res) => {
+    res.send("Backend Running Successfully");
+});
+
+// Get Tasks
 app.get("/tasks", (req, res) => {
-  res.json(tasks);
+    res.json(tasks);
 });
 
+// Add Task
 app.post("/tasks", (req, res) => {
-  tasks.push(req.body);
-  res.json({ message: "Task Added" });
+    tasks.push(req.body);
+    res.json({
+        message: "Task Added"
+    });
 });
 
+// Update Task
 app.put("/tasks/:id", (req, res) => {
-  tasks[req.params.id] = req.body;
-  res.json({ message: "Task Updated" });
+    tasks[req.params.id] = req.body;
+
+    res.json({
+        message: "Task Updated"
+    });
 });
 
+// Delete Task
 app.delete("/tasks/:id", (req, res) => {
-  tasks.splice(req.params.id, 1);
-  res.json({ message: "Task Deleted" });
+
+    tasks.splice(req.params.id, 1);
+
+    res.json({
+        message: "Task Deleted"
+    });
+
 });
 
 app.listen(5000, () => {
-  console.log("Server Running");
+    console.log("Server Running");
 });
