0e948d87622dee002478f0b5626fbcdca65839f0
priyayadav25 <priyay2519@gmail.com>
2026-06-15 21:23:08 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,32 +1,13 @@
-function register(){
-alert("Registration Successful");
-window.location.href="login.html";
-}
-
-function login(){
-alert("Login Successful");
-window.location.href="dashboard.html";
-}
-
 function addTask(){
 
 let task=document.getElementById("taskInput").value;
 
-if(task===""){
-alert("Enter Task");
-return;
-}
-
 let li=document.createElement("li");
 
-li.innerHTML=`
-${task}
-<button onclick="this.parentElement.remove()">
-Delete
-</button>
-`;
+li.innerHTML=
+task+
+` <button onclick="this.parentElement.remove()">Delete</button>`;
 
 document.getElementById("taskList").appendChild(li);
 
-document.getElementById("taskInput").value="";
 }
