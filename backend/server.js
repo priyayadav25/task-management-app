fef0cb9c96694fca0f25d87e055e1c9fcca51665
priyayadav25 <priyay2519@gmail.com>
2026-06-15 21:20:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,32 @@
-const express=require("express");
-const cors=require("cors");
+const express = require("express");
+const cors = require("cors");
 
-const app=express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-res.send("Backend Running");
+let tasks = [];
+
+app.get("/tasks", (req, res) => {
+  res.json(tasks);
 });
 
-app.listen(5000,()=>{
-console.log("Server running on port 5000");
+app.post("/tasks", (req, res) => {
+  tasks.push(req.body);
+  res.json({ message: "Task Added" });
+});
+
+app.put("/tasks/:id", (req, res) => {
+  tasks[req.params.id] = req.body;
+  res.json({ message: "Task Updated" });
+});
+
+app.delete("/tasks/:id", (req, res) => {
+  tasks.splice(req.params.id, 1);
+  res.json({ message: "Task Deleted" });
+});
+
+app.listen(5000, () => {
+  console.log("Server Running");
 });
