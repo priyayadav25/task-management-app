c6cd57f889650bf0f7d327d53b41cb08e0ce8c36
priyayadav25 <priyay2519@gmail.com>
2026-06-16 13:43:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,84 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-let tasks = [];
+const DATA_FILE = "./data.json";
+
+/* Load Tasks */
+
+function getTasks() {
+    const data = fs.readFileSync(DATA_FILE, "utf8");
+    return JSON.parse(data);
+}
+
+/* Save Tasks */
+
+function saveTasks(tasks) {
+    fs.writeFileSync(
+        DATA_FILE,
+        JSON.stringify(tasks, null, 2)
+    );
+}
+
+/* Home */
 
 app.get("/", (req, res) => {
     res.send("Backend Running Successfully");
 });
 
+/* Get Tasks */
+
 app.get("/tasks", (req, res) => {
+
+    const tasks = getTasks();
+
     res.json(tasks);
 });
 
+/* Add Task */
+
 app.post("/tasks", (req, res) => {
+
+    const tasks = getTasks();
+
     tasks.push(req.body);
+
+    saveTasks(tasks);
 
     res.json({
         message: "Task Added"
     });
 });
 
+/* Update Task */
+
 app.put("/tasks/:id", (req, res) => {
+
+    const tasks = getTasks();
+
     tasks[req.params.id] = req.body;
+
+    saveTasks(tasks);
 
     res.json({
         message: "Task Updated"
     });
 });
 
+/* Delete Task */
+
 app.delete("/tasks/:id", (req, res) => {
+
+    const tasks = getTasks();
+
     tasks.splice(req.params.id, 1);
+
+    saveTasks(tasks);
 
     res.json({
         message: "Task Deleted"
