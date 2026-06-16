6326746628c78b19fd476dafc7891006a6518b90
priyayadav25 <priyay2519@gmail.com>
2026-06-16 12:56:44 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,29 +1,7 @@
-async function addTask() {
-    const taskInput = document.getElementById("taskInput");
-    const task = taskInput.value;
+const API_URL = "http://localhost:5000/tasks";
 
-    if (!task) {
-        alert("Enter Task");
-        return;
-    }
-
-    await fetch("http://localhost:5000/tasks", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            title: task,
-            completed: false
-        })
-    });
-
-    taskInput.value = "";
-    loadTasks();
-}
-
-async function loadTasks() {
-    const response = await fetch("http://localhost:5000/tasks");
+async function displayTasks() {
+    const response = await fetch(API_URL);
     const tasks = await response.json();
 
     const taskList = document.getElementById("taskList");
@@ -33,24 +11,75 @@ async function loadTasks() {
         const li = document.createElement("li");
 
         li.innerHTML = `
-            ${task.title}
-            <button onclick="deleteTask(${index})">Delete</button>
+            <span class="${task.completed ? 'completed' : ''}">
+                ${task.title}
+            </span>
+
+            <button onclick="completeTask(${index})">
+                Done
+            </button>
+
+            <button onclick="deleteTask(${index})">
+                Delete
+            </button>
         `;
 
         taskList.appendChild(li);
     });
 }
 
+async function addTask() {
+    const taskInput = document.getElementById("taskInput");
+
+    if (taskInput.value.trim() === "") {
+        alert("Enter a task");
+        return;
+    }
+
+    await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: taskInput.value,
+            completed: false
+        })
+    });
+
+    taskInput.value = "";
+
+    displayTasks();
+}
+
+async function completeTask(index) {
+    const response = await fetch(API_URL);
+    const tasks = await response.json();
+
+    tasks[index].completed = true;
+
+    await fetch(`${API_URL}/${index}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(tasks[index])
+    });
+
+    displayTasks();
+}
+
 async function deleteTask(index) {
-    await fetch(`http://localhost:5000/tasks/${index}`, {
+    await fetch(`${API_URL}/${index}`, {
         method: "DELETE"
     });
 
-    loadTasks();
+    displayTasks();
 }
 
 function logout() {
+    alert("Logged Out");
     window.location.href = "login.html";
 }
 
-window.onload = loadTasks;
+displayTasks();
